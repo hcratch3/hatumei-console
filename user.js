4a1edfa27f60bf4aed49f3ace8630add1c842838
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2024-11-04 17:07:40 +0900

    Create user.js

---
+++ user.js
@@ -0,0 +1,14 @@
+var config = {
+  apiKey: "AIzaSyAz9Sk2JEjKWn7882ehr-paId_YtvkFJEg",
+  authDomain: "toyota-hatumei-login-0001.firebaseapp.com",
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    document.getElementById('userH').innerText = user;
+    console.log('auth user', user);
+  } else {
+    setTimeout(function(){window.location.href = 'index.html';}, 5*1000);
+  }
+});
